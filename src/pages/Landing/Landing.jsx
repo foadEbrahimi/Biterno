a150fca0300d11c6e995d75d60ba3c42c12ae6c5
Foad <fwad40901@gmail.com>
2025-03-31 19:22:51 +0330

    add crypto to landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Hero from '../../pages/Landing/Hero';
 import Arz from '../../pages/Landing/Arz';
+import Crypto from './Crypto';
 
 export default function Landing() {
   return (
-    <div className='relative'>
+    <div className="relative">
       <Hero />
       <Arz />
+      <Crypto />
     </div>
   );
 }
