2e2893b307c99137d8ea7361411b26f1286f8375
Foad <fwad40901@gmail.com>
2025-03-31 23:54:39 +0330

    create HowWork Card

---
+++ src/components/Landing/howItWork/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Card() {
+  return (
+    <div className="text-center max-w-[25rem]">
+      <span className="text-[#777E90] font-semibold">مرحله یک</span>
+      <h2 className="text-lg font-bold mt-5">دانلود</h2>
+      <p className="text-[#353945]">
+        ما ایده ها را از ساده تا پیچیده درک می کنیم، استفاده از همه چیز آسان می
+        شود
+      </p>
+    </div>
+  );
+}
