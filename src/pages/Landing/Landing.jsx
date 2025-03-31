a62fc7ab943ca59967882c10b3b78881ade06b78
Foad <fwad40901@gmail.com>
2025-03-31 14:27:31 +0330

    create Landing

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../../components/Landing/Navbar';
+import Hero from '../../components/Landing/Hero';
+
+export default function Landing() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
