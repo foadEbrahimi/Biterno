a063ac5f41644046c7e80118b1d6212b44c28125
Foad <fwad40901@gmail.com>
2025-04-04 15:07:51 +0330

    create CardSlider/Market

---
+++ src/components/Market/CardSlider.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import chart from "../../assets/imgs/chart.svg";
+import bitcoin from "../../assets/imgs/coin symbol (2).svg";
+export default function CardSlider() {
+  return (
+    <div className="flex w-fit items-start gap-5" dir="ltr">
+      <div className="flex items-start gap-3">
+        <img src={bitcoin} alt="svg" />
+        <div className="flex flex-col">
+          <div className="flex items-center gap-2">
+            <span className="font-sans font-semibold text-[#777E90]">
+              BTC/USDT
+            </span>
+            <span
+              dir="ltr"
+              className="flex items-center justify-center rounded-full bg-[#58BD7D] px-2 font-sans text-white"
+            >
+              {" "}
+              +0.79%
+            </span>
+          </div>
+          <span className="font-sans text-3xl font-bold">36,641.20</span>
+          <span className="font-sans text-lg font-light">36,641.20</span>
+        </div>
+      </div>
+      <img src={chart} className="hidden xl:flex" alt="chart svg" />
+    </div>
+  );
+}
