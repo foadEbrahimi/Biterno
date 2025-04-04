615af244f2c806a4b2b300ba21ac39c095a748c2
Foad <fwad40901@gmail.com>
2025-04-04 17:31:50 +0330

    fix Cryptocurrencies

--- src/components/Market/Cryptocurrencies.jsx
+++ src/components/Market/Cryptocurrencies.jsx
@@ -12,7 +12,7 @@ import Menu from "../../components/Market/Menu";
 
 export default function Cryptocurrencies() {
   return (
-    <div className="z-10 mt-[19rem] w-full rounded-[24px] border bg-white pt-5 shadow-2xl lg:mt-[10rem]">
+    <div className="z-10 mt-[19rem] w-full rounded-[24px] border bg-white pt-5 shadow-2xl lg:mt-[13rem] xl:mt-[10rem]">
       <div className="grid place-items-center gap-2 px-5 pb-[26px] sm:grid-cols-2 lg:grid-cols-3">
         <CardSlider />
       </div>
