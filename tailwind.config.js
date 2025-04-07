2fe7e6625f44a1dc69b21d28c41b44b94b771cf3
Foad <fwad40901@gmail.com>
2025-04-08 00:09:34 +0330

    fix: reorganize Tailwind CSS configuration for improved readability and structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,38 +4,36 @@ export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", extract],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			arial: [
-  				'Arial'
-  			],
-  			vazir: [
-  				'vazir'
-  			]
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		}
-  	}
+    screens,
+    fontSize,
+    extend: {
+      fontFamily: {
+        arial: ["Arial"],
+        vazir: ["vazir"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [fluid, require("tailwindcss-animate")],
 };
