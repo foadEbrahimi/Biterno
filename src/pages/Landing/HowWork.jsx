015b506e805863bcb9a01ec408ec2e8bca443fc0
Foad <fwad40901@gmail.com>
2025-04-08 00:09:29 +0330

    fix: standardize import statements and improve layout structure in HowWork component

--- src/pages/Landing/HowWork.jsx
+++ src/pages/Landing/HowWork.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import Layout from '../../layout/landing/Layout';
-import Card from '../../components/Landing/howItWork/Card';
+import Layout from "../../layout/landing/Layout";
+import Card from "../../components/Landing/howItWork/Card";
 
-import connectLine from '../../assets/imgs/connect-line.svg';
-import step1 from '../../assets/imgs/step-1.png';
-import step2 from '../../assets/imgs/step-2.png';
-import step3 from '../../assets/imgs/step-3.png';
-import step4 from '../../assets/imgs/step-4.png';
+import connectLine from "../../assets/imgs/connect-line.svg";
+import step1 from "../../assets/imgs/step-1.png";
+import step2 from "../../assets/imgs/step-2.png";
+import step3 from "../../assets/imgs/step-3.png";
+import step4 from "../../assets/imgs/step-4.png";
 
 export default function HowWork() {
   return (
-    <div className="mt-20 pb-20">
+    <div className="mt-20 flex h-[100dvh] items-center pb-20">
       <div>
         <Layout>
           <div>
-            <div className="flex items-center flex-col justify-center gap-2">
-              <span className="~text-3xl/4xl font-bold text-center">
+            <div className="flex flex-col items-center justify-center gap-2">
+              <span className="text-center font-bold ~text-4xl/5xl">
                 چگونه کار می کند
               </span>
-              <p className="font-semibold text-[#353945] text-center max-w-[30rem]">
+              <p className="mt-4 max-w-[30rem] text-center !leading-[2rem] font-semibold text-[#353945]">
                 Stacks یک کتابخانه آماده تولید از بلوک های محتوای stackable است
                 که در React Native ساخته شده است.
               </p>
             </div>
-            <div className="mt-10 items-center hidden md:flex justify-center gap-10 mx-auto">
+            <div className="mx-auto mt-10 hidden items-center justify-center gap-10 md:flex">
               <img src={step1} className="w-10 lg:w-20" alt="" />
               <img
                 src={connectLine}
@@ -45,7 +45,7 @@ export default function HowWork() {
               />
               <img src={step4} className="w-10 lg:w-20" alt="" />
             </div>
-            <div className="mt-10 grid md:grid-cols-4 gap-5">
+            <div className="mt-10 grid gap-5 md:grid-cols-4">
               {/* TODO: add slider */}
               <Card />
               <Card />
