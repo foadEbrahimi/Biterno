b59209851ba7e140ec45808b1222fa2b95397016
Foad <fwad40901@gmail.com>
2025-04-07 22:09:30 +0330

    fix: enhance layout and styling of Contact component for improved responsiveness

--- src/pages/Landing/Contact.jsx
+++ src/pages/Landing/Contact.jsx
@@ -1,32 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import Layout from '../../layout/landing/Layout';
-import Card from '../../components/Landing/contact/Card';
+import Card from "../../components/Landing/contact/Card";
+import Layout from "../../layout/landing/Layout";
+import arrow from "../../assets/imgs/arrow-long.svg";
 
 export default function Contact() {
   return (
-    <div className="mt-20">
+    <div className="mt-20 flex min-h-[100dvh] items-center bg-[#F4F5F6] py-20">
       <Layout>
         <div className="flex flex-col items-center">
-          <span className="~text-3xl/4xl font-bold">
+          <span className="mb-4 max-w-[23rem] text-center font-bold !leading-[3rem] ~text-3xl/4xl">
             در چند ثانیه یک معامله گر کریپتو شوید
           </span>
-          <p className="text-[#777E90] font-semibold lg:hidden text-sm mt-3">
+          <p className="text-sm font-semibold text-[#777E90] lg:hidden">
             Stacks یک کتابخانه آماده تولید از بلوک های محتوای قابل استک است که
             در React Native ساخته شده است.
           </p>
-          <p className="text-[#777E90] hidden lg:flex font-semibold mt-3">
-            ما همه چیزهایی را که برای شروع معامله نیاز دارید در اختیار داریم.{' '}
+          <p className="mt-3 hidden font-semibold text-[#777E90] lg:flex">
+            ما همه چیزهایی را که برای شروع معامله نیاز دارید در اختیار
+            داریم.{" "}
           </p>
         </div>
-        <div className="grid lg:grid-cols-3 items-center justify-center mt-10 gap-10">
+        <div className="mt-16 grid items-center justify-center lg:grid-cols-3">
           {/* TODO: add slider */}
           <Card />
-          <Card />
-          <Card />
         </div>
-        <button className="bg-blue-600 mx-auto mt-5 text-white py-1.5 flex px-6 rounded-full font-semibold text-lg">
-          ارتباط با ما{' '}
+        <div className="mt-8 flex items-center justify-center gap-3 lg:hidden">
+          <button className="flex h-10 w-10 items-center justify-center rounded-full hover:border-2">
+            <img src={arrow} alt="" className="w-4" />
+          </button>
+          <button className="flex h-10 w-10 items-center justify-center rounded-full hover:border-2">
+            <img src={arrow} alt="" className="w-4 scale-x-[-1]" />
+          </button>
+        </div>
+        <button className="mx-auto mt-7 flex w-full items-center justify-center rounded-full bg-blue-600 px-7 py-2.5 text-center text-lg font-semibold text-white transition-all duration-200 hover:bg-[#0045ea] lg:w-fit xl:mt-14">
+          ارتباط با ما{" "}
         </button>
       </Layout>
     </div>
