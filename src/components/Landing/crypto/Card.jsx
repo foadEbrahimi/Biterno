ef4536d7c25c4e39a16afce3f313cf0319438935
Foad <fwad40901@gmail.com>
2025-04-08 20:38:39 +0330

    fix Crypto/Card

--- src/components/Landing/crypto/Card.jsx
+++ src/components/Landing/crypto/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Card({ height, vertical, img, max = false }) {
   return (
     <div
-      className={`group cursor-pointer space-y-3 overflow-hidden ${
+      className={`group w-full cursor-pointer space-y-3 overflow-hidden ${
         vertical === true
           ? "flex flex-row-reverse items-center gap-5"
           : "flex flex-col"
@@ -16,13 +16,15 @@ export default function Card({ height, vertical, img, max = false }) {
           className={`rounded-2xl transition-all duration-700 group-hover:scale-110 ${height} w-full`}
         />
       </div>
-      <div className={`space-y-2 ${max && "max-w-[15rem]"}`}>
+      <div
+        className={`flex flex-col items-end space-y-2 lg:items-start ${max && "max-w-[15rem]"}`}
+      >
         <span
-          className={`text-[#23262F] ${vertical ? "" : "text-2xl"} font-bold transition-all duration-300 group-hover:text-[#3772FF]`}
+          className={`text-[#23262F] ${vertical ? "" : "text-lg lg:text-2xl"} text-right font-bold transition-all duration-300 group-hover:text-[#3772FF]`}
         >
           توکن های اهرمی اکنون در دسترس هستند
         </span>
-        <p className="text-xs font-semibold text-[#777E90]">
+        <p className="max-w-[20rem] text-right text-xs font-semibold leading-6 text-[#777E90] lg:!min-w-full lg:text-sm">
           چیزهای خوب در 3 ثانیه می آیند. اکنون 3 برابر توکن اهرمی دریافت
           کنید.{" "}
         </p>
