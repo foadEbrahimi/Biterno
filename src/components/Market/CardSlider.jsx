815936fdbf14f13db6cc1406b8befc926c6501ef
Foad <fwad40901@gmail.com>
2025-04-09 22:47:55 +0330

    refactor CardSlider component to implement Swiper for improved sliding functionality

--- src/components/Market/CardSlider.jsx
+++ src/components/Market/CardSlider.jsx
@@ -2,29 +2,110 @@ import React from "react";
 
 import chart from "../../assets/imgs/chart.svg";
 import bitcoin from "../../assets/imgs/coin symbol (2).svg";
+
+// Import Swiper styles
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-fade";
+
 export default function CardSlider() {
   return (
-    <div className="flex w-fit items-start gap-5" dir="ltr">
-      <div className="flex items-start gap-3">
-        <img src={bitcoin} alt="svg" />
-        <div className="flex flex-col">
-          <div className="flex items-center gap-2">
-            <span className="font-sans font-semibold text-[#777E90]">
-              BTC/USDT
-            </span>
-            <span
-              dir="ltr"
-              className="flex items-center justify-center rounded-full bg-[#58BD7D] px-2 text-white"
-            >
-              {" "}
-              +0.79%
-            </span>
+    <Swiper
+      autoplay={{
+        delay: 1500,
+        disableOnInteraction: false,
+      }}
+      slidesPerView={1.3}
+      loop
+      spaceBetween={10}
+      modules={[Navigation, Autoplay]}
+      navigation={{
+        nextEl: ".nextSlide",
+        prevEl: ".prevSlide",
+      }}
+      breakpoints={{
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      }}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <div className="flex w-fit items-start gap-5" dir="ltr">
+          <div className="flex items-start gap-3">
+            <img src={bitcoin} alt="bitcoin svg" />
+            <div className="flex flex-col">
+              <div className="flex items-center gap-2">
+                <span className="font-sans font-semibold text-[#777E90]">
+                  BTC/USDT
+                </span>
+                <span
+                  dir="ltr"
+                  className="flex items-center justify-center rounded-full bg-[#58BD7D] px-2 text-white"
+                >
+                  {" "}
+                  +0.79%
+                </span>
+              </div>
+              <span className="mt-1 text-3xl font-bold">36,641.20</span>
+              <span className="text-lg font-light">36,641.20</span>
+            </div>
           </div>
-          <span className="mt-1 text-3xl font-bold">36,641.20</span>
-          <span className="text-lg font-light">36,641.20</span>
+          <img src={chart} className="hidden xl:flex" alt="chart svg" />
         </div>
-      </div>
-      <img src={chart} className="hidden xl:flex" alt="chart svg" />
-    </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="flex w-fit items-start gap-5" dir="ltr">
+          <div className="flex items-start gap-3">
+            <img src={bitcoin} alt="bitcoin svg" />
+            <div className="flex flex-col">
+              <div className="flex items-center gap-2">
+                <span className="font-sans font-semibold text-[#777E90]">
+                  BTC/USDT
+                </span>
+                <span
+                  dir="ltr"
+                  className="flex items-center justify-center rounded-full bg-[#58BD7D] px-2 text-white"
+                >
+                  {" "}
+                  +0.79%
+                </span>
+              </div>
+              <span className="mt-1 text-3xl font-bold">36,641.20</span>
+              <span className="text-lg font-light">36,641.20</span>
+            </div>
+          </div>
+          <img src={chart} className="hidden xl:flex" alt="chart svg" />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="flex w-fit items-start gap-5" dir="ltr">
+          <div className="flex items-start gap-3">
+            <img src={bitcoin} alt="bitcoin svg" />
+            <div className="flex flex-col">
+              <div className="flex items-center gap-2">
+                <span className="font-sans font-semibold text-[#777E90]">
+                  BTC/USDT
+                </span>
+                <span
+                  dir="ltr"
+                  className="flex items-center justify-center rounded-full bg-[#58BD7D] px-2 text-white"
+                >
+                  {" "}
+                  +0.79%
+                </span>
+              </div>
+              <span className="mt-1 text-3xl font-bold">36,641.20</span>
+              <span className="text-lg font-light">36,641.20</span>
+            </div>
+          </div>
+          <img src={chart} className="hidden xl:flex" alt="chart svg" />
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 }
