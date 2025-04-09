2a76678e3166e84ad75e784676b1a7fffd96a46b
Foad <fwad40901@gmail.com>
2025-04-09 23:41:48 +0330

    refactor LearnCard component for improved layout and image handling

--- src/components/Market/LearnCard.jsx
+++ src/components/Market/LearnCard.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import img from "../../assets/imgs/blog/media container.png";
+import img from "../../assets/imgs/lessons-pic-1.jpg";
 
 export default function LearnCard() {
   return (
-    <div>
-      <img src={img} className="w-full" alt="" />
+    <div className="group cursor-pointer lg:border-b lg:pb-12">
+      <div className="overflow-hidden rounded-xl">
+        <img
+          src={img}
+          className="h-[14rem] w-full transition-all duration-700 group-hover:scale-110"
+          alt="learn img"
+        />
+      </div>
       <div className="mt-4 flex flex-col gap-3">
         <div className="w-fit rounded-lg bg-[#FF6838] px-3 py-1 font-semibold text-white">
           جدید
         </div>
-        <span className="text-lg font-bold text-[#23262F]">
+        <span className="text-lg font-bold text-[#23262F] transition-all duration-200 group-hover:text-[#3772FF]">
           درباره سکه UI8 بیاموزید و یک گذرنامه All-Access کسب کنید
         </span>
       </div>
