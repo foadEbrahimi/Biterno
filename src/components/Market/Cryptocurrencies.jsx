4414481cd35cf8697c87c25fe3cf00e53d6f6b13
Foad <fwad40901@gmail.com>
2025-04-09 22:47:26 +0330

    refactor Cryptocurrencies component for improved layout and localization

--- src/components/Market/Cryptocurrencies.jsx
+++ src/components/Market/Cryptocurrencies.jsx
@@ -12,23 +12,19 @@ import {
 
 export default function Cryptocurrencies() {
   return (
-    <div className="absolute -bottom-[4.8rem] lg:-bottom-[5rem] left-0 w-full ~px-5/32">
+    <div className="absolute -bottom-[4.8rem] left-0 w-full ~px-5/32 lg:-bottom-[5rem]">
       <div className="z-10 w-full rounded-[24px] border bg-white pt-5 shadow-2xl">
-        <div className="grid place-items-center gap-2 px-5 pb-[26px] sm:grid-cols-2 lg:grid-cols-3">
+        <div className="px-5 pb-[26px]">
           <CardSlider />
         </div>
         <div className="border-t px-5 py-5">
           <div className="lg:hidden">
-            <Select>
-              <SelectTrigger className="font-sans">
-                <SelectValue placeholder="Cryptocurrencies" />
+            <Select dir="rtl">
+              <SelectTrigger>
+                <SelectValue placeholder="ارزهای رمزنگاری شده" />
               </SelectTrigger>
-              <SelectContent className="bg-white font-sans">
-                <SelectItem value="DeFi">DeFi</SelectItem>
-                <SelectItem value="Innovation">Innovation</SelectItem>
-                <SelectItem value="POS">POS</SelectItem>
-                <SelectItem value="NFT">NFT</SelectItem>
-                <SelectItem value="POW">POW</SelectItem>
+              <SelectContent className="bg-white">
+                <SelectItem value="crypto">ارزهای رمزنگاری شده</SelectItem>
               </SelectContent>
             </Select>
           </div>
