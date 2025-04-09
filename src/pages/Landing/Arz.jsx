112b77e36004887ec957bb1401c52f479f0e013c
Foad <fwad40901@gmail.com>
2025-04-09 17:42:02 +0330

    add slider to Arz

--- src/pages/Landing/Arz.jsx
+++ src/pages/Landing/Arz.jsx
@@ -1,16 +1,72 @@
-import React from 'react';
-import Card from '../../components/Landing/arz/Card';
-import Layout from '../../layout/landing/Layout';
+import React from "react";
+
+import Card from "../../components/Landing/arz/Card";
+import Layout from "../../layout/landing/Layout";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import { Autoplay, EffectCoverflow, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function Arz() {
   return (
-    <Layout>
-      <div className="bg-[#F4F5F6] grid sm:grid-cols-2 py-10 lg:grid-cols-3 xl:grid-cols-4 gap-2 rounded-[24px] p-3">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    </Layout>
+    // <Layout>
+    <div className="mb-36 mt-10 gap-2 p-3 py-5 md:mt-5 lg:mt-0">
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"1.5"}
+        breakpoints={{
+          768: {
+            slidesPerView: 2.3,
+          },
+          1024: {
+            slidesPerView: 3.3,
+          },
+          1285: {
+            slidesPerView: 3.7,
+          },
+          1520: {
+            slidesPerView: 4.5,
+          },
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 50,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination, Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Card />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Card />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Card />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Card />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Card />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Card />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+    // </Layout>
   );
 }
