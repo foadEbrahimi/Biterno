bc61d7edb4a488ec74171ac396932ff51adc1036
Foad <fwad40901@gmail.com>
2025-04-09 23:41:41 +0330

    refactor Learn component for improved layout and spacing

--- src/components/Market/Learn.jsx
+++ src/components/Market/Learn.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 
-import Layout from "../../layout/landing/Layout";
 import LearnCard from "./LearnCard";
 
 export default function Learn() {
   return (
-    <div className="mt-20">
+    <div className="my-20 flex items-center lg:h-[90dvh]">
       <div>
-        <div className="flex flex-col gap-3">
-          <span className="text-center font-bold text-[#23262F] ~text-3xl/4xl">
+        <div className="flex flex-col gap-5">
+          <span className="text-center font-bold text-[#23262F] ~text-2xl/4xl">
             یاد بگیرید و کسب درآمد کنید
           </span>
-          <span className="mx-auto text-center text-[#353945] md:max-w-[20rem]">
+          <span className="mx-auto text-center text-sm text-[#353945] md:max-w-[20rem]">
             Stacks is a production-ready library of stackablecontent blocks
             built in React Native.
           </span>
         </div>
-        <div className="mt-5 grid gap-5 md:grid-cols-3 lg:gap-10">
+        <div className="mt-12 grid gap-10 md:grid-cols-3 lg:gap-10">
           <LearnCard />
           <LearnCard />
           <LearnCard />
         </div>
+        <button className="mx-auto mt-10 flex items-center justify-center gap-5 rounded-full border-2 px-5 py-2 transition-all duration-200 hover:border-[#23262F] hover:bg-[#23262F] hover:text-[#FCFCFD]">
+          مشاهده بیشتر
+          <div className="loader-market"></div>
+        </button>
       </div>
     </div>
   );
