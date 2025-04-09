e747ffda02a9d6580b733503e7c7736db59e27ba
Foad <fwad40901@gmail.com>
2025-04-09 22:21:53 +0330

    add Trade to Landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import Hero from '../../pages/Landing/Hero';
-import Arz from '../../pages/Landing/Arz';
-import Crypto from './Crypto';
-import MarketTrend from './MarketTrend';
-import Contact from './Contact';
-import Subs from './Subs';
-import HowWork from './HowWork';
+import Hero from "../../pages/Landing/Hero";
+import Arz from "../../pages/Landing/Arz";
+import Crypto from "./Crypto";
+import MarketTrend from "./MarketTrend";
+import Contact from "./Contact";
+import Subs from "./Subs";
+import HowWork from "./HowWork";
+import Trade from "./Trade";
 
 export default function Landing() {
   return (
@@ -16,6 +17,7 @@ export default function Landing() {
       <Crypto />
       <MarketTrend />
       <Contact />
+      <Trade />
       <Subs />
       <HowWork />
     </div>
