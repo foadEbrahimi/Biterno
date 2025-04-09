9143ab7fa1a2c040a9809503d8e724e7a3dcdf05
Foad <fwad40901@gmail.com>
2025-04-09 18:17:43 +0330

    clear Hero codes

--- src/pages/Landing/Hero.jsx
+++ src/pages/Landing/Hero.jsx
@@ -1,18 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Content from "../../components/Landing/hero/Content";
 
-import chevronBottom from "../../assets/imgs/chevronBottom.svg";
-import { Link } from "react-router";
-import { useLocalState } from "../../context/LocalStateProvider";
-
 export default function Hero() {
-  const { showNav } = useLocalState();
-
   return (
     <div className="relative">
       <Content />
-      
     </div>
   );
 }
