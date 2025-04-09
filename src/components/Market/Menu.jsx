2930bf9a601ee71b37e3d4d6bfaf95c53c59b5e0
Foad <fwad40901@gmail.com>
2025-04-09 22:39:03 +0330

    refactor Menu component for localization and styling improvements

--- src/components/Market/Menu.jsx
+++ src/components/Market/Menu.jsx
@@ -2,31 +2,15 @@ import React from "react";
 
 export default function Menu() {
   return (
-    <div
-      dir="ltr"
-      className="hidden font-sans lg:flex lg:items-center lg:justify-between"
-    >
+    <div className="hidden lg:flex lg:items-center lg:justify-between">
       <div className="lg:flex lg:items-center lg:gap-3">
-        <span className="flex h-9 items-center justify-center rounded-full bg-[#353945] px-4 pb-0.5 font-semibold text-[#FCFCFD]">
-          Cryptocurrencies
-        </span>
-        <span className="flex h-9 items-center justify-center rounded-full px-4 pb-0.5 font-semibold text-[#353945]">
-          DeFi
-        </span>
-        <span className="flex h-9 items-center justify-center rounded-full px-4 pb-0.5 font-semibold text-[#353945]">
-          Innovation
-        </span>
-        <span className="flex h-9 items-center justify-center rounded-full px-4 pb-0.5 font-semibold text-[#353945]">
-          POS
-        </span>
-        <span className="flex h-9 items-center justify-center rounded-full px-4 pb-0.5 font-semibold text-[#353945]">
-          NFT
-        </span>
-        <span className="flex h-9 items-center justify-center rounded-full px-4 pb-0.5 font-semibold text-[#353945]">
-          POW
+        <span className="flex h-9 items-center justify-center rounded-full bg-[#353945] px-4 pb-0.5 text-sm font-semibold text-[#FCFCFD]">
+          ارزهای رمزنگاری شده
         </span>
       </div>
-      <button className="rounded-full border-2 px-3 py-1 text-lg">Trade</button>
+      <button className="rounded-full border-2 px-4 py-1.5 text-lg transition-all duration-200 hover:border-[#23262F] hover:bg-[#23262F] hover:text-[#FCFCFD]">
+        معامله
+      </button>
     </div>
   );
 }
