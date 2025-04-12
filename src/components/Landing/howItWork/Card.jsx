137bfac7557be7d69dcd0c1dc606cf6b40029edc
Foad <fwad40901@gmail.com>
2025-04-12 09:29:14 +0330

    Refactor Card / howItWorks

--- src/components/Landing/howItWork/Card.jsx
+++ src/components/Landing/howItWork/Card.jsx
@@ -1,26 +1,22 @@
 import React from "react";
 
 import connectLine from "../../../assets/imgs/connect-line.svg";
-import step1 from "../../../assets/imgs/step-1.png";
 
-export default function Card() {
+export default function Card({ image, step, title, desc }) {
   return (
     <div className="flex w-fit max-w-[15rem] flex-col gap-5 text-right lg:text-center 2xl:max-w-[18rem]">
       <div className="flex items-center gap-5 xl:hidden">
-        <img src={step1} className="w-24" alt="" />
+        <img src={image} className="~w-20/24" alt="step img" />
         <img
           src={connectLine}
-          className="flex-1 md:w-52 md:flex-none lg:w-fit"
+          className="w-32 md:w-52 md:flex-none lg:w-fit"
           alt="connectLine svg"
         />
       </div>
       <div>
-        <span className="font-semibold text-[#777E90]">مرحله یک</span>
-        <h2 className="my-3 mt-5 text-lg font-bold">دانلود</h2>
-        <p className="text-[#353945]">
-          ما ایده ها را از ساده تا پیچیده درک می کنیم، استفاده از همه چیز آسان
-          می شود
-        </p>
+        <span className="font-vazirDemiBold text-[#777E90]">مرحله {step}</span>
+        <h2 className="my-3 mt-5 text-lg font-bold">{title}</h2>
+        <p className="text-[#353945]">{desc}</p>
       </div>
     </div>
   );
