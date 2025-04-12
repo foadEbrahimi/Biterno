5ef1694a860e7779ca6b7d8a3964543f52ee7825
Foad <fwad40901@gmail.com>
2025-04-12 09:29:28 +0330

    Add howItWork constant and refactor HowWork component to utilize it for step display

---
+++ src/constants/howItWork.js
@@ -0,0 +1,31 @@
+import step1 from "../assets/imgs/step-1.png";
+import step2 from "../assets/imgs/step-2.png";
+import step3 from "../assets/imgs/step-3.png";
+import step4 from "../assets/imgs/step-4.png";
+
+export const howItWork = [
+  {
+    step: "یک",
+    title: "دانلود",
+    desc: "ما ایده ها را از ساده تا پیچیده درک می کنیم، استفاده از همه چیز آسان می شود",
+    image: step1,
+  },
+  {
+    step: "دو",
+    title: "کیف پول را وصل کنید",
+    desc: "ما ایده ها را از ساده تا پیچیده درک می کنیم، استفاده از همه چیز آسان می شود",
+    image: step2,
+  },
+  {
+    step: "سوم",
+    title: "تجارت را شروع کنید",
+    desc: "ما ایده ها را از ساده تا پیچیده درک می کنیم، استفاده از همه چیز آسان می شود",
+    image: step3,
+  },
+  {
+    step: "چهارم",
+    title: "کسب درآمد کنید",
+    desc: "ما ایده ها را از ساده تا پیچیده درک می کنیم، استفاده از همه چیز آسان می شود",
+    image: step4,
+  },
+];
