bd9d8508867bdf1f923e2ecc35c1806a0981ea8d
Foad <fwad40901@gmail.com>
2025-04-14 00:48:41 +0330

    re-design card in hero

--- src/features/Landing/arz/components/Card.jsx
+++ src/features/Landing/arz/components/Card.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import chart from "@assets/imgs/chart.svg";
 import chart2 from "@assets/imgs/chart2.svg";
 
-import { formatToTomanWithCommas } from "../../../../utils/helper";
 import { Skeleton } from "@/components/ui/Skeleton";
+import { formatToTomanWithCommas } from "../../../../utils/helper";
 
 export function ArzCardSkeleton() {
   return (
@@ -26,34 +26,38 @@ export function ArzCardSkeleton() {
   );
 }
 
-export default function CardArz({ name, image, price, change24h }) {
+export default function CardArz({ name, image, price }) {
   return (
-    <div
-      className="flex w-full cursor-pointer items-center justify-between gap-5 rounded-2xl border p-5 py-6 transition-all duration-300 hover:bg-white"
-      dir="ltr"
-    >
-      <div className="flex flex-1 flex-col gap-1">
-        <img src={image} alt={`${name} coin img`} className="w-10" />
-        <div>
-          <div className="flex flex-col items-start gap-2">
-            <div className="flex items-center gap-2">
-              <span className="font-sans font-semibold text-[#777E90]">
-                {name}/USDT
-              </span>
-              <span
-                className={`rounded-full ${change24h < 0 ? "bg-red-600" : "bg-green-600"} px-4 py-1 text-sm text-white`}
-              >
-                {change24h}%
-              </span>
-            </div>
-            <span className="text-2xl font-semibold text-[#23262F]">
-              {formatToTomanWithCommas(price?.slice(0, -1))}
-            </span>
-          </div>
+    <div className="flex h-[8.5rem] !w-full cursor-pointer items-center justify-between gap-5 rounded-full bg-white px-10 transition-all duration-300">
+      <div className="flex flex-col items-start gap-2">
+        <div className="flex items-center gap-2">
+          <img src={image} className="w-8" alt="" />
+          <span className="font-vazirDemiBold text-sm uppercase text-gray-500">
+            {name}/usdt
+          </span>
+        </div>
+        <div className="flex items-center gap-3">
+          <span className="text-2xl font-bold">
+            {formatToTomanWithCommas(price)}
+          </span>
+          <svg
+            aria-hidden="true"
+            data-prefix="fal"
+            data-icon="arrow-up"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            className="drop-shadow-green-600 w-4 text-green-600 drop-shadow-2xl"
+          >
+            <path
+              fill="currentColor"
+              d="M4.465 263.536l7.07 7.071c4.686 4.686 12.284 4.686 16.971 0L207 92.113V468c0 6.627 5.373 12 12 12h10c6.627 0 12-5.373 12-12V92.113l178.494 178.493c4.686 4.686 12.284 4.686 16.971 0l7.07-7.071c4.686-4.686 4.686-12.284 0-16.97l-211.05-211.05c-4.686-4.686-12.284-4.686-16.971 0L4.465 246.566c-4.687 4.686-4.687 12.284 0 16.97z"
+            ></path>
+          </svg>
         </div>
       </div>
-      <div className="hidden flex-1 items-center justify-center md:flex">
-        <img src={change24h < 0 ? chart2 : chart} alt="" />
+      <div className="">
+        <img src={chart} className="" alt="" />
       </div>
     </div>
   );
