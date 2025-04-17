4edf214bc7f74e4346066b4240dec767045c9816
Foad <fwad40901@gmail.com>
2025-04-17 20:24:33 +0330

    fix card in Market

--- src/features/market/components/Card.jsx
+++ src/features/market/components/Card.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
+import { formatToTomanWithCommas } from "@/utils/helper";
 import chart from "@assets/imgs/chart.svg";
 import chart2 from "@assets/imgs/chart2.svg";
-import { formatToTomanWithCommas } from "@/utils/helper";
 
 export default function Card({ image, name, price, change24h }) {
   return (
     <div
-      className="group/hover flex w-fit cursor-pointer items-start gap-5"
+      className="group/hover flex !w-fit cursor-pointer items-start gap-5"
       dir="rtl"
     >
       <div className="flex items-start gap-3">
-        <img src={image} className="w-10" alt="bitcoin svg" />
+        <img src={image} className="w-8" alt="bitcoin svg" />
         <div className="flex flex-col">
           <div className="flex items-center gap-2">
             <span className="font-sans font-semibold text-[#777E90]">
@@ -19,16 +19,16 @@ export default function Card({ image, name, price, change24h }) {
             </span>
             <span
               dir="ltr"
-              className={`flex items-center justify-center rounded-full py-0.5 text-sm ${
-                change24h > 0 ? "bg-green-600" : "bg-red-600"
-              } px-2 text-white`}
+              className={`flex items-center justify-center rounded-full py-0.5 font-vazirDemiBold text-sm ${
+                change24h > 0 ? "text-green-600" : "text-red-600"
+              } px-2`}
             >
               {" "}
               {change24h}
             </span>
           </div>
-          <span className="mt-1 font-vazirBold text-2xl transition-all duration-200 group-hover/hover:text-[#3772FF]">
-            {formatToTomanWithCommas(price.slice(0, -1))}
+          <span className="mt-1 flex items-center gap-2 font-vazirBold text-lg transition-all duration-200 group-hover/hover:text-[#3772FF]">
+            {formatToTomanWithCommas(price.slice(0, -1))} <span>تومان</span>
           </span>
         </div>
       </div>
