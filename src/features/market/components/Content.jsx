43fdf04cd82f1d5ab8fa410b1f133f600fc2f0d5
Foad <fwad40901@gmail.com>
2025-04-17 20:24:56 +0330

    fix content in Market

--- src/features/market/components/Content.jsx
+++ src/features/market/components/Content.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
-import market from "@assets/imgs/market-pic.png";
+import market from "@assets/imgs/market/chart.svg";
 
 export default function Content() {
   return (
-    <div className="mt-10 flex flex-col gap-5 sm:flex-row sm:items-center lg:mb-24 lg:mt-0 xl:mb-32">
+    <div className="mt-20 flex flex-col gap-5 sm:flex-row sm:items-center lg:mb-24 lg:mt-0 xl:mb-32">
       <div className="flex flex-col ~gap-5/9 sm:flex-1">
         <h1 className="font-bold !leading-[3rem] ~text-4xl/6xl xl:!leading-[4.5rem]">
-          قیمت <br /> ارزهای دیجیتال <br /> امروزی
+          قیمت لحظه ای <br /> ارزهای دیجیتال
         </h1>
         <span className="text-[#353945] ~text-sm/2xl">
           ارزش بازار جهانی کریپتو{" "}
-          <span className="font-extrabold text-black">1.86 تریلیون دلار</span>{" "}
+          <span className="font-extrabold text-primary">1.86 تریلیون دلار</span>{" "}
           است
         </span>
       </div>
-      <div className="w-full sm:flex-[1.5]">
+      <div className="w-full pt-5 sm:flex-[1.5] xl:flex-1">
         <img src={market} alt="market img" />
       </div>
     </div>
