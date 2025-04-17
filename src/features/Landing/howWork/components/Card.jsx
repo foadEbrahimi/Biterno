0d805825639f0b3180f9886bbee9792a674c864d
Foad <fwad40901@gmail.com>
2025-04-17 17:47:07 +0330

    fix howork card

--- src/features/Landing/howWork/components/Card.jsx
+++ src/features/Landing/howWork/components/Card.jsx
@@ -4,13 +4,13 @@ import connectLine from "@assets/imgs/connect-line.svg";
 
 export default function Card({ image, step, title, desc, index }) {
   return (
-    <div className="flex w-fit max-w-[15rem] flex-col gap-5 text-right lg:text-center 2xl:max-w-[18rem]">
-      <div className="flex items-center gap-5 xl:hidden">
-        <img src={image} className={`~w-20/24`} alt="step img" />
+    <div className="flex w-fit mr-10 max-w-[15rem] flex-col gap-5 text-right lg:text-center 2xl:max-w-[22rem]">
+      <div className="flex items-center gap-5 lg:hidden">
+        <img src={image} className={`~w-12/24`} alt="step img" />
         <img
           src={connectLine}
           className={`w-32 md:w-52 md:flex-none lg:w-fit ${
-            index === 4 && "hidden"
+            index === 3 && "hidden"
           }`}
           alt="connectLine svg"
         />
