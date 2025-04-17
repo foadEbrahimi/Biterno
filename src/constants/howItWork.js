7796ba65156e54daf8553050edd5d35151bd8e20
Foad <fwad40901@gmail.com>
2025-04-17 17:46:46 +0330

    add how constants

--- src/constants/howItWork.js
+++ src/constants/howItWork.js
@@ -1,31 +1,24 @@
-import step1 from "../assets/imgs/step-1.png";
-import step2 from "../assets/imgs/step-2.png";
-import step3 from "../assets/imgs/step-3.png";
-import step4 from "../assets/imgs/step-4.png";
+import step1 from "../assets/imgs/how/step1.svg";
+import step2 from "../assets/imgs/how/step2.svg";
+import step3 from "../assets/imgs/how/step3.svg";
 
 export const howItWork = [
   {
     step: "یک",
-    title: "دانلود",
-    desc: "ما ایده ها را از ساده تا پیچیده درک می کنیم، استفاده از همه چیز آسان می شود",
-    image: step1,
+    title: "ثبت نام",
+    desc: "با ثبت نام در بیترنو ماجراجویی خود را اغاز کنید.",
+    image: step3,
   },
   {
     step: "دو",
-    title: "کیف پول را وصل کنید",
-    desc: "ما ایده ها را از ساده تا پیچیده درک می کنیم، استفاده از همه چیز آسان می شود",
+    title: "احزارهویت",
+    desc: "در کمتر از 30 ثانیه احرازهویت خود را تکمیل و از خدمات بیترنو استفاده کنید.",
     image: step2,
   },
   {
     step: "سوم",
-    title: "تجارت را شروع کنید",
-    desc: "ما ایده ها را از ساده تا پیچیده درک می کنیم، استفاده از همه چیز آسان می شود",
-    image: step3,
-  },
-  {
-    step: "چهارم",
-    title: "کسب درآمد کنید",
-    desc: "ما ایده ها را از ساده تا پیچیده درک می کنیم، استفاده از همه چیز آسان می شود",
-    image: step4,
+    title: "تایید نهایی",
+    desc: "تبریک میگوییم ! هم اکنون میتوانید از خدمات بیترنو استفاده کنید.",
+    image: step1,
   },
 ];
