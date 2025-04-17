2610008cb2f6cfd612907992091ee0a38ce917b1
Foad <fwad40901@gmail.com>
2025-04-17 11:16:34 +0330

    add subs constants

---
+++ src/constants/subs.js
@@ -0,0 +1,15 @@
+import news from "@assets/imgs/news.svg";
+import news2 from "@assets/imgs/news2.svg";
+
+export const subs = [
+  {
+    title: "قیمت لحظه ای رمز ارزها",
+    desc: "با ما در کانال تلگرام همراه باشید و از اخرین قیمت رمز ارزها مطلع شوید",
+    img: news,
+  },
+  {
+    title: "اخرین اخبار و اطلاعات وبسایت",
+    desc: "به کانال تلگرام با بپیوندید و از جدیدترین اطلاع رسانی های بیترنو اگاه شوید",
+    img: news2,
+  },
+];
