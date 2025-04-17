7a2ed7ed1c379adf106a9453ddd04bddddee7959
Foad <fwad40901@gmail.com>
2025-04-17 20:48:44 +0330

    fix Market

--- src/features/market/Market.jsx
+++ src/features/market/Market.jsx
@@ -6,18 +6,27 @@ import Cryptocurrencies from "./components/Cryptocurrencies";
 import Learn from "./components/Learn";
 import Table from "./components/Table";
 import SmallCards from "./components/SmallCards";
+import Navbar from "@/components/layout/landing/Navbar";
+
+import img from "@assets/imgs/hero/object.png";
+import MarketTrend from "../Landing/marketTrend/MarketTrend";
 
 export default function Market() {
   return (
-    <div>
+    <div className="overflow-x-hidden">
+      <Navbar />
       <div
         style={{
           height: "calc(100dvh - 56px)",
         }}
-        className="relative bg-[#DECBE9] p-2 py-10 lg:flex lg:items-center"
+        className="relative p-2 py-10 lg:flex lg:items-center"
       >
+        <div className="absolute right-0 top-0 !-z-10 h-full w-full">
+          <img src={img} alt="" />
+        </div>
+        <div className="absolute -left-20 -top-20 !-z-20 h-[326px] w-[322px] rounded-full bg-primary/40 blur-[100px]"></div>
         <Layout>
-          <div className="">
+          <div className="mt-5">
             <Content />
             <Cryptocurrencies />
           </div>
@@ -25,10 +34,10 @@ export default function Market() {
       </div>
       <Layout>
         <div className="~mt-32/40 lg:hidden">
-          <SmallCards />
+          <MarketTrend bg={false} maxCrypto={10} />
         </div>
         <Table />
-        <Learn />
+        {/* <Learn /> */}
       </Layout>
     </div>
   );
