1637e1d4f4d63b6bbd8a58da26e425c0d1ec6eaa
Foad <fwad40901@gmail.com>
2025-04-18 22:20:37 +0330

    refactor contact constants and update Card component to include new features and improve image responsiveness

--- src/features/Landing/contact/components/Card.jsx
+++ src/features/Landing/contact/components/Card.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Card({ title, desc, img }) {
   return (
     <div className="rounded-2xl bg-white p-3 px-5 pb-12 hover:shadow-xl xl:max-w-[25rem]">
-      <img src={img} className="mx-auto h-[10rem] flex pt-10" alt="" />
+      <img src={img} className="mx-auto h-[10rem] flex max-w-[40%] pt-10" alt="" />
       <div className="mt-10 space-y-2 pb-12 text-center">
         <span className="text-xl font-bold">{title}</span>
         <p dir="rtl" className="!pt-3 text-center font-vazirMedium text-[#777E90]">
