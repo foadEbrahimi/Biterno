de952839469027a6fda9b1444a69beb4b4980995
Foad <fwad40901@gmail.com>
2025-04-18 21:26:56 +0330

    test3

--- src/features/Landing/subs/Subs.jsx
+++ src/features/Landing/subs/Subs.jsx
@@ -17,7 +17,7 @@ export default function Subs() {
   return (
     <div className="pb-20 pt-10">
       <Layout>
-        <div className="flex flex-col items-center">
+        {/* <div className="flex flex-col items-center">
           <div className="flex items-center gap-1">
             <img src={logo} alt="logo svg" className="w-20" />
           </div>
@@ -25,7 +25,7 @@ export default function Subs() {
             با بیترنو از اخرین تحولات بازار رمز ارزها مطلع باشید
           </span>
           <p className="mt-4 text-[#777E90]">دسترسی لحظه ای به اطلاعات بازار</p>
-        </div>
+        </div> */}
         <div className="relative !mx-auto ~mt-10/16 md:mx-10 md:max-w-[80%] xl:h-[30rem]">
           <button className="nextSlide absolute -bottom-8 left-[38%] flex h-10 w-10 translate-x-[-50%] translate-y-[50%] items-center justify-center rounded-full border-2 md:-left-10 md:bottom-[50%]">
             <img src={arrow} alt="arrow svg" className="w-4 scale-x-[-1]" />
