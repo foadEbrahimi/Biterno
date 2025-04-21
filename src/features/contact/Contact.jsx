d1443d52084b71f43c370512290bb994d85f9987
Foad <fwad40901@gmail.com>
2025-04-21 17:05:31 +0330

    re-design contact

--- src/features/contact/Contact.jsx
+++ src/features/contact/Contact.jsx
@@ -1,95 +1,99 @@
 import React, { useState } from "react";
 
-import arrow from "../../assets/imgs/common/arrow-long.svg";
-import contactImg from "../../assets/imgs/contact/contact-pic.png";
-
 import Layout from "../../components/layout/landing/Layout";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-fade";
-import { Navigation } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import Card from "./components/Card";
 import Navbar from "@/components/layout/landing/Navbar";
+
+import object from "@assets/imgs/landing/hero/object.png";
 
 export default function Contact() {
   const [state, setState] = useState("general");
 
   return (
-    <div className="py-20 2xl:my-0">
+    <div className="relative py-20 lg:py-14 2xl:my-0">
+      <img
+        src={object}
+        className="absolute right-0 top-0 -z-10 h-full !min-w-[250%] lg:!min-w-[110%]"
+        alt=""
+      />
       <Navbar />
       <Layout>
-        <div className="flex w-full flex-col items-start gap-5 md:flex-row">
-          <div className="flex w-full flex-1 flex-col ~gap-6/10 lg:pt-10 xl:pt-20">
-            <h1 className="~text-4xl/6xl">سوالات متداول</h1>
-            <ul className="flex items-center gap-2 text-sm [&>*]:rounded-full [&>*]:px-4 [&>*]:py-1.5">
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <div className="flex w-full flex-1 flex-col items-center ~gap-6/10 lg:pt-10 xl:pt-20">
+            <h1 className="!leading-[4rem] ~text-4xl/5xl">
+              <span className="text-primary">بیترنو</span> نسل نوین معاملات
+              هوشمند
+            </h1>
+            <ul className="flex items-center gap-2 text-[9px] lg:text-[18px] [&>*]:rounded-full [&>*]:px-3 [&>*]:py-1.5">
               <li
                 onClick={() => setState("general")}
-                className={`cursor-pointer ${state === "general" ? "bg-[#353945] font-semibold text-white" : "text-[#777E90] hover:text-[#353945]"}`}
+                className={`cursor-pointer ${state === "general" ? "bg-primary font-semibold text-white" : "hover:text-primabg-primary text-[#777E90]"}`}
               >
-                عمومی
+                احرازهویت
               </li>
               <li
                 onClick={() => setState("support")}
-                className={`cursor-pointer ${state === "support" ? "bg-[#353945] font-semibold text-white" : "text-[#777E90] hover:text-[#353945]"}`}
+                className={`cursor-pointer ${state === "support" ? "bg-primary font-semibold text-white" : "hover:text-primabg-primary text-[#777E90]"}`}
               >
-                پشتیبانی
+                درباره ما
               </li>
               <li
                 onClick={() => setState("hosting")}
-                className={`cursor-pointer ${state === "hosting" ? "bg-[#353945] font-semibold text-white" : "text-[#777E90] hover:text-[#353945]"}`}
+                className={`cursor-pointer ${state === "hosting" ? "bg-primary font-semibold text-white" : "hover:text-primabg-primary text-[#777E90]"}`}
               >
-                میزبانی
+                کارمزد
               </li>
               <li
                 onClick={() => setState("product")}
-                className={`cursor-pointer ${state === "product" ? "bg-[#353945] font-semibold text-white" : "text-[#777E90] hover:text-[#353945]"}`}
+                className={`cursor-pointer ${state === "product" ? "bg-primary font-semibold text-white" : "hover:text-primabg-primary text-[#777E90]"}`}
               >
-                محصول
+                قوانین و شرایط
+              </li>
+              <li
+                onClick={() => setState("product")}
+                className={`cursor-pointer ${state === "tellUs" ? "bg-primary font-semibold text-white" : "hover:text-primabg-primary text-[#777E90]"}`}
+              >
+                تماس با ما
               </li>
             </ul>
-            <div className="mt-5 flex flex-col gap-5">
-              <div className="group flex cursor-pointer items-center gap-3 border-b pb-5">
-                <span className="text-lg text-[#777E90] transition-all duration-300 group-hover:text-[#3772FF]">
-                  01
-                </span>
-                <span className="font-bold transition-all duration-300 group-hover:text-[#3772FF]">
-                  چگونه کار می کند؟
-                </span>
-              </div>
-              <div className="group flex cursor-pointer items-center gap-3 border-b pb-5">
-                <span className="text-lg text-[#777E90] transition-all duration-300 group-hover:text-[#3772FF]">
-                  01
-                </span>
-                <span className="font-bold transition-all duration-300 group-hover:text-[#3772FF]">
-                  چگونه کار می کند؟
-                </span>
-              </div>
-              <div className="group flex cursor-pointer items-center gap-3 border-b pb-5">
-                <span className="text-lg text-[#777E90] transition-all duration-300 group-hover:text-[#3772FF]">
-                  01
-                </span>
-                <span className="font-bold transition-all duration-300 group-hover:text-[#3772FF]">
-                  چگونه کار می کند؟
-                </span>
-              </div>
-              <div className="group flex cursor-pointer items-center gap-3 pb-5">
-                <span className="text-lg text-[#777E90] transition-all duration-300 group-hover:text-[#3772FF]">
-                  01
-                </span>
-                <span className="font-bold transition-all duration-300 group-hover:text-[#3772FF]">
-                  چگونه کار می کند؟
-                </span>
-              </div>
+            <div className="flex flex-col gap-5 rounded-[30px] border p-10 text-right text-[16px] leading-10 text-black/80">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+              استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+              در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
+              نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+              کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان
+              جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای
+              طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان
+              فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری
+              موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد
+              نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل
+              دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.لورم ایپسوم متن
+              ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان
+              گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان
+              که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای
+              متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت
+              و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
+              طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+              الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این
+              صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
+              شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی
+              دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی
+              اساسا مورد استفاده قرار گیرد.لورم ایپسوم متن ساختگی با تولید سادگی
+              نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و
+              متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای
+              شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود
+              ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال
+              و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم
+              افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان
+              خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان
+              امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت
+              تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی،
+              و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده
+              قرار گیرد.
             </div>
-          </div>
-          <div className="flex-[2]">
-            <img src={contactImg} alt="contact png" />
           </div>
         </div>
       </Layout>
-      <div className="mt-20 w-full">
+      {/* <div className="mt-20 w-full">
         <span className="flex justify-center text-center font-bold leading-8 text-[#777E90] ~px-5/40">
           قبل از درخواست پاسخ، ممکن است آن را در مرکز یادگیری رمزنگاری ما بیابید
         </span>
@@ -139,7 +143,7 @@ export default function Contact() {
             </div>
           </Swiper>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
