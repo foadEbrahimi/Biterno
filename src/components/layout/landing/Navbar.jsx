e470d11081b56145eb3950643a863f60ddfb61d9
Foad <fwad40901@gmail.com>
2025-04-22 19:11:52 +0330

    fix navbar

--- src/components/layout/landing/Navbar.jsx
+++ src/components/layout/landing/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, NavLink } from "react-router";
 
 import chevronBottom from "@assets/imgs/landing/hero/chevronBottom.svg";
-import englandFlag from "@assets/imgs/landing/hero/england-flag.webp";
+// import englandFlag from "@assets/imgs/landing/hero/england-flag.webp";
 import iranFlag from "@assets/imgs/landing/hero/iran-flag.png";
 import logo from "@assets/imgs/logo/biterno-b.svg";
 
@@ -43,7 +43,7 @@ export default function Navbar() {
                 </div>
               </Link>
             </div>
-            <div>
+            <div className="~mr-0/32">
               <ul className="hidden items-center gap-5 divide-x-2 divide-x-reverse font-bold text-[#777E90] md:flex [&>*]:cursor-pointer">
                 <NavLink
                   to="/auth/login"
@@ -119,7 +119,7 @@ export default function Navbar() {
                 </DropdownMenuContent> */}
               </DropdownMenu>
               {theme === "light" ? (
-                <button className="ml-3" onClick={() => setTheme("dark")}>
+                <button className="ml-2" onClick={() => setTheme("dark")}>
                   <svg
                     width="24"
                     height="24"
@@ -148,7 +148,7 @@ export default function Navbar() {
                   <svg
                     xmlnsXlink="http://www.w3.org/2000/svg"
                     viewBox="0 0 384 512"
-                    className="w-5 fill-[#777E90] transition-all duration-200 hover:fill-textColor"
+                    className="w-5 fill-[#646464] transition-all duration-200 hover:fill-textColor"
                   >
                     <path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z" />
                   </svg>
