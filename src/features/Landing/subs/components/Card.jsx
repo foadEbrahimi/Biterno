27b11296fe9ad63c1dd66d2164fddc7a2a5771b2
Foad <fwad40901@gmail.com>
2025-04-25 21:09:05 +0330

    maybe fix the subs card img :)

--- src/features/Landing/subs/components/Card.jsx
+++ src/features/Landing/subs/components/Card.jsx
@@ -10,20 +10,18 @@ export default function Card({ title, desc, img }) {
         <span className="text-center font-bold !leading-[2.5rem] ~text-2xl/4xl dark:text-white lg:text-right">
           {title}
         </span>
-        <span className="font-iranSansDemiBold max-w-[15rem] text-center text-sm text-[#777E90] lg:min-w-fit lg:text-right">
+        <span className="max-w-[15rem] text-center font-iranSansDemiBold text-sm text-[#777E90] lg:min-w-fit lg:text-right">
           {desc}
         </span>
-        <button className="font-iranSansDemiBold mt-10 w-fit rounded-full border-2 px-4 py-2.5 text-[16px] text-black transition-all duration-200 hover:border-primary hover:bg-primary hover:text-[#FCFCFD] dark:text-white">
+        <button className="mt-10 w-fit rounded-full border-2 px-4 py-2.5 font-iranSansDemiBold text-[16px] text-black transition-all duration-200 hover:border-primary hover:bg-primary hover:text-[#FCFCFD] dark:text-white">
           اکنون عضو شوید
         </button>
       </div>
-      <div className="order-1 mt-6 flex-1 lg:order-2">
-        <img
-          src={img}
-          alt=""
-          className="mr-auto h-[9rem] w-full object-cover lg:h-[10rem] lg:w-fit xl:h-full xl:w-[70%]"
-        />
-      </div>
+      <img
+        src={img}
+        alt=""
+        className="order-1 mx-auto mr-auto mt-6 h-[5rem] w-[10rem] flex-1 object-cover lg:order-2 lg:h-full lg:!max-w-[14rem] xl:h-fit xl:!max-w-[16rem]"
+      />
     </div>
   );
 }
